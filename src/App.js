100e6efa9364387ecfc2df25bd2b396b0ebe6938
Daniel Brazel <dbrazel4@gmail.com>
2018-11-28 12:32:58 -0500

    new

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <p>"What do you want to count?"</p>
         </header>
       </div>
     );
